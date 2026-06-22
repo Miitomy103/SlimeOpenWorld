e330214376bc8bab7233b35814efdf295c7304fd
Miitomy103 <tomy463463@gmail.com>
2026-06-22 17:40:26 +0900

    汎用スクリプトをCommonフォルダへ移動
    プロジェクト固有の型に依存しない汎用スクリプト(ObjectPool, OverLap, StateMachine, Rotator, Move, Direction, Orbit, AnimationEventAction, InputKeyEventAction, Editor用InterfaceChanger/TodoList)をAssets/Script/Commonへ移動。移動時に不要なusing(Unity.VisualScripting, 未使用のUnityEngine等)を削除。

--- Assets/Script/Common/StateMachine/StateTransition/IStateTransition.cs
+++ Assets/Script/Common/StateMachine/StateTransition/IStateTransition.cs
@@ -1,5 +1,3 @@
-using UnityEngine;
-
 namespace StateMachine
 {
     public interface IStateTransition<TKey>
--- Assets/Script/Common/StateMachine/TransitionCondition/ITransitionCondition.cs
+++ Assets/Script/Common/StateMachine/TransitionCondition/ITransitionCondition.cs
@@ -1,5 +1,3 @@
-using UnityEngine;
-
 namespace StateMachine
 {
     public interface ITransitionCondition<TKey>
