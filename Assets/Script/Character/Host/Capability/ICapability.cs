25232e9e56d365963649011fdaacbe9db9c0c385
Miitomy103 <tomy463463@gmail.com>
2026-06-22 18:02:08 +0900

    未使用スクリプトを削除
    非汎用かつ未使用と判明したスクリプトを削除。
    MonoBehaviour継承クラスはFindUsage.ps1(GUID参照)、非継承クラス(interface/抽象クラス)はクラス名の全文grepで使用箇所ゼロを確認した上で削除。
    スタブ(NotImplementedException)、空テンプレ、実装が他クラスに置き換わって参照されなくなったもの、放置されたinterfaceを対象とした。

--- Assets/Script/Character/Host/Capability/ICapability.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface ICapability
-{
-    bool IsWait { get; }
-    void HandleInput(PlayerInput input);
-}
--- Assets/Script/Quest/QuestTransition/QuestTransitionBase.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using UnityEngine;
-
-public abstract class QuestTransitionBase : MonoBehaviour, IQuestTransition
-{
-    public string TargetQuestID => throw new NotImplementedException();
-
-    protected abstract void TransitionQuest();
-}
